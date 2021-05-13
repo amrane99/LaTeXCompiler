b1b1f861c8369c99e9f4901e0abca938875d5864
Amin Ranem <aminranem@googlemail.com>
2021-05-13 21:28:18 +0200

    Added readme as description for PyPi.

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import pathlib
 from setuptools import setup, find_packages
+
+# The directory containing this file]
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 setup(
   name = 'LatexCompiler',         # How you named package folder (LatexCompiler)
@@ -9,6 +16,8 @@ setup(
   version = '1.0',     # Start with a small number and increase it with every change you make
   license='MIT',       # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A program for compiling LaTeX files the convenient way by stashing the auxiliary files into a specified folder.',   # Give a short description about library
+  long_description=README,
+  long_description_content_type="text/markdown",
   author = 'Amin Ranem',                   # Type in name
   author_email = 'aminranem@googlemail.com',      # Type in E-Mail
   url = 'https://github.com/amrane99/',   # Provide either the link to github or to own website
