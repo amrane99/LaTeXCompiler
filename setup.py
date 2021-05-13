c77653a48827b0ea87bd196ab61e300b5ce67ee1
Amin Ranem <aminranem@googlemail.com>
2021-05-13 21:20:10 +0200

    Fixed wrong PyPi classifier.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
       ],
   classifiers=[
     'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers, Users',      # Define that your audience are developers
+    'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
   ],
